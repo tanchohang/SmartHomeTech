32708599fe62fc4729c2c6d5c465ef4311098e4f
tanchohang <tanchohang@gmail.com>
2021-02-16 13:25:48 +0000

    landing page wired

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -3,5 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 
 
+def home(request):
+    return render(request, 'accounts/landing.html')
+
+
 def login(request):
     return render(request, 'accounts/login.html')
