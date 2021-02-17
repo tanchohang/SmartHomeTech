5f9f9ebc3fb8d227d4eedb645c60975f2174cab7
tanchohang <tanchohang@gmail.com>
2021-02-17 13:50:46 +0000

    new html files added

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -4,5 +4,13 @@ from . import views
 urlpatterns = [
     path('', views.home),
     path('login/', views.login),
+    path('products/', views.products),
+    path('portfolio/', views.portfolio),
+    path('about/', views.about),
+    path('contact/', views.contact),
+    path('estimator/', views.estimator),
+
+
+
 
 ]
